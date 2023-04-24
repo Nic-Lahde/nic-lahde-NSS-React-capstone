d67c67bffe1afd61f82184c72fb51ff40af293ad
Nic Lahde <nic.lahde@gmail.com>
2023-04-24 11:41:45 -0500

    added hovers to images + an easter egg

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,20 +1,20 @@
 import { Outlet, Route, Routes } from "react-router-dom"
 import { Profile } from "../users/Profile"
-import { MyGames} from "../users/MyGames"
+import { MyGames } from "../users/MyGames"
 import { CreateGame } from "../users/CreateGame"
-import {HostEvent} from "../events/HostEvent"
-import {FindEvent} from "../events/FindEvent"
-import {MyEvents} from "../events/MyEvents"
+import { HostEvent } from "../events/HostEvent"
+import { FindEvent } from "../events/FindEvent"
+import { MyEvents } from "../events/MyEvents"
 import { EventDetails } from "../events/EventDetails"
 export const ApplicationViews = () => {
-    return (
+    return (<>
         <Routes>
             <Route path="/" element={
                 <>
                     <div className="main--header">
-                        <img alt="two dice" src="https://www.publicdomainpictures.net/pictures/380000/nahled/two-dice-fly-on-the-rool-with-shado.png"/>
+                        <img className="main--header--image" alt="two dice" src="https://www.publicdomainpictures.net/pictures/380000/nahled/two-dice-fly-on-the-rool-with-shado.png" />
                         <h1>The Kitchen Table</h1>
-                        <img alt="four meeples" src="../images/meeples.png"/>
+                        <img className="main--header--image" alt="four meeples" src="../images/meeples.png" />
                     </div>
                     <h3>Connecting meeples since 2023</h3>
 
@@ -22,13 +22,16 @@ export const ApplicationViews = () => {
                 </>
             }>
 
-                <Route path="profile" element={ <Profile /> } />
-                <Route path="myGames" element={ < MyGames />} />
-                <Route path="myGames/createGame" element={  <CreateGame />   }/>
-                <Route path="hostEvent" element={ <HostEvent /> } />
-                <Route path="findEvent" element={ <><MyEvents/> <FindEvent /></> } />
-                <Route path="findEvent/:eventId" element={ <><MyEvents/><EventDetails /><FindEvent/></> } />
+                <Route path="profile" element={<Profile />} />
+                <Route path="myGames" element={< MyGames />} />
+                <Route path="myGames/createGame" element={<CreateGame />} />
+                <Route path="hostEvent" element={<HostEvent />} />
+                <Route path="findEvent" element={<><MyEvents /> <FindEvent /></>} />
+                <Route path="findEvent/:eventId" element={<><MyEvents /><EventDetails /><FindEvent /></>} />
             </Route>
         </Routes>
+        <img className="cool--s" alt="a very cool letter S" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Cool_S.svg/450px-Cool_S.svg.png?20211026093152" />
+    </>
     )
+
 }
